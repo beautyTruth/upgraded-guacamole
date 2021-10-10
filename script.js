04127225305af00c31c676a3a5c1c485b5f91d6f
Phil <happy.melodist@gmail.com>
2021-10-09 23:34:06 -0500

    complete footer html markup

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
+
+const newYear = document.querySelector(".year");
+const yearUpdate = new Date().getFullYear();
+newYear.textContent = yearUpdate;
